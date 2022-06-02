c09668f53c4a8faf7fd7a8e0ca9cb967fdd590ad
Faustino Galceran <faustinogalceran@gmail.com>
2022-06-01 23:58:34 -0300

    commit entregable N°4

--- grh-muebles/src/App.js
+++ grh-muebles/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import BsNavBar from './components/BsNavBar';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return <BsNavBar/>;
+         <ItemListContainer greeting={'Hola mundo'}>;
 
 }
 
